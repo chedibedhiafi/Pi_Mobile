66233e09a3353536c15b7f7ce847b921a2fc3de8
Haroun Jemaa <haroun.jemaa@esprit.tn>
2022-05-12 23:48:32 +0200

    mobile
    mobile

---
+++ src/com/mycompany/myapp/ModifForm.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp;
+
+import com.codename1.ui.Button;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Form;
+import com.codename1.ui.TextField;
+import com.codename1.ui.layouts.BoxLayout;
+import com.company.entites.Blog;
+
+/**
+ *
+ * @author chedi
+ */
+public class ModifForm extends Form {
+    public ModifForm(int id){
+         setTitle("modifier-blog");
+        setLayout(BoxLayout.y());
+        TextField blogTitre = new TextField("","donner un tire pour le Blog");
+        TextField blogSujet = new TextField("","donner un sujet pour le blog");
+        TextField blogContenu = new TextField("","donner un contenu pour le blog");
+        TextField blogDate = new TextField("","donner une date pour le blog");
+        Button btn = new Button("modifier");
+        
+      
+      //  categ.setNom(nom);
+        this.addAll(blogTitre,blogSujet,blogContenu,blogDate,btn);
+    
+        btn.addActionListener(l->{
+        //  System.out.println( );
+        ServiceBlog.getInstance().modifierBlog(id, blogTitre.getText(),blogSujet.getText() ,blogContenu.getText(),blogDate.getText());
+           Dialog.show("succe", "modifier  avec succer", "ok", "cancel");
+        Form listform = new ListForm();
+    listform.show();
+        });
+    }
+}
